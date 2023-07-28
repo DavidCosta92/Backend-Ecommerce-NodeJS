e4380375cfa254629e8bf145d93e2bcb808bf98f
David Costa <89800442+DavidCosta92@users.noreply.github.com>
2023-07-27 22:56:39 -0300

    Modificacion de parametros getLoguedUser()

--- src/middlewares/authenticator.js
+++ src/middlewares/authenticator.js
@@ -21,49 +21,49 @@ export function onlyAuthenticatedApi(req, res, next) {
 }
 export async function onlyAdminOrPremiumApi(req, res, next) {    
   // solo corrobora rol, el owner sera chequeado a nivel de servicios
-  let user = await userService.getLoguedUser(req , next)
+  let user = await userService.getLoguedUser(req, res , next)
   if(user?.role === "user") return next(new AuthorizationError ("Debes ser usuario PREMIUM o Administrador"))
   next()
 }
 export async function onlyAdminOrPremiumWeb(req, res, next) {    
   // solo corrobora rol, el owner sera chequeado a nivel de servicios
-  let user = await userService.getLoguedUser(req , next)
+  let user = await userService.getLoguedUser(req, res , next)
   if(user?.role === "user") return next(new AuthorizationErrorWEB ("Debes ser usuario PREMIUM o Administrador"))
   next()
 }
 export async function onlyAdminWeb(req, res, next) {    
-  let user = await userService.getLoguedUser(req , next)
+  let user = await userService.getLoguedUser(req, res , next)
   if(user?.role !== "admin") return next(new AuthorizationErrorWEB ("Debes ser administrador"))
   next()
 }
 export async function onlyAdminApi(req, res, next) {    
-  let user = await userService.getLoguedUser(req , next)
+  let user = await userService.getLoguedUser(req, res , next)
   if(user?.role !== "admin") return next(new AuthorizationError ("Debes ser administrador"))
   next()
 }
 export async function onlyUserApi(req, res, next) {
-  let user = await userService.getLoguedUser(req , next)
+  let user = await userService.getLoguedUser(req, res , next)
   if(user?.role !== "user") return next(new AuthorizationError ("Debes ser USUARIO"))
   next()
 }
 export async function onlyUserWeb(req, res, next) {
-  let user = await userService.getLoguedUser(req , next)
+  let user = await userService.getLoguedUser(req, res , next)
   if(user?.role !== "user") return next(new AuthorizationErrorWEB ("Debes ser USUARIO"))
   next()
 }
 export async function notAdminApi(req, res, next) {
-  let user = await userService.getLoguedUser(req , next)
+  let user = await userService.getLoguedUser(req, res , next)
   if(user?.role === "admin") return next(new AuthorizationError ("Un administrador no puede realizar esta accion"))
   next()
 }
 export async function notAdminWeb(req, res, next) {
-  let user = await userService.getLoguedUser(req , next)
+  let user = await userService.getLoguedUser(req, res , next)
   if(user?.role === "admin") return next(new AuthorizationErrorWEB ("Un administrador no puede realizar esta accion"))
   next()
 }
 export async function renderHome(req , res , next){
   try {
-    let user = await userService.getLoguedUser(req , next)
+    let user = await userService.getLoguedUser(req, res , next)
     if(user === undefined){
       res.render("home", {loguedUser :false}) 
     } else {
